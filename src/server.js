56fdc4f4b80ea6e7c3f0429b5f7f7a4b26a3c767
BouilleJames <77192627+BouilleJames@users.noreply.github.com>
2023-06-14 14:53:19 +0200

    add test

--- src/server.js
+++ src/server.js
@@ -25,45 +25,45 @@ app.get("/articles", (req, res) => {
   });
 });
 
-app.post("/articles", (req, res) => {
-  const author = req.body.author;
-  const content = req.body.content;
-  if (!author) {
-    res.status(400).json({ error: "L'auteur est obligatoire" });
-    return;
-  }
-  if (!content) {
-    res.status(400).json({ error: "Le message est obligatoire" });
-    return;
-  }
+// app.post("/articles", (req, res) => {
+//   const author = req.body.author;
+//   const content = req.body.content;
+//   if (!author) {
+//     res.status(400).json({ error: "L'auteur est obligatoire" });
+//     return;
+//   }
+//   if (!content) {
+//     res.status(400).json({ error: "Le message est obligatoire" });
+//     return;
+//   }
 
-  db.query(
-    "INSERT INTO articles(author, content) VALUES(?, ?)",
-    [author, content],
-    (error, data) => {
-      if (error) {
-        console.error(error);
-        res.status(500).send("Erreur du serveur");
-      } else {
-        res.status(201).json({ message: "message créé avec succès" });
-      }
-    }
-  );
-});
+//   db.query(
+//     "INSERT INTO articles(author, content) VALUES(?, ?)",
+//     [author, content],
+//     (error, data) => {
+//       if (error) {
+//         console.error(error);
+//         res.status(500).send("Erreur du serveur");
+//       } else {
+//         res.status(201).json({ message: "message créé avec succès" });
+//       }
+//     }
+//   );
+// });
 
-app.put("/articles/", (req, res) => {
-  const author = req.body.author;
-  const content = req.body.content;
-  console.log(content);
-  connection.query(
-    "UPDATE articles SET content = ? WHERE author = ?;",
-    [content, author],
-    (err, results) => {
-      if (err) throw err;
-      res.json(results);
-    }
-  );
-});
+// app.put("/articles/", (req, res) => {
+//   const author = req.body.author;
+//   const content = req.body.content;
+//   console.log(content);
+//   connection.query(
+//     "UPDATE articles SET content = ? WHERE author = ?;",
+//     [content, author],
+//     (err, results) => {
+//       if (err) throw err;
+//       res.json(results);
+//     }
+//   );
+// });
 
 app.patch("/articles/", (req, res) => {
   const author = req.body.author;
